dc748b83be4f969e8167dea0eb6d72725e2bea84
big-fredo <www.kitchahferdinand.com@gmail.com>
2024-10-14 20:26:42 +0100

    Customer class

---
+++ Customer.java
@@ -0,0 +1,27 @@
+package learn;
+
+public class Customer {
+
+String customerId;
+String name;
+
+public Customer(String customerId, String name) {
+
+this.customerId = customerId;
+this.name = name;
+
+}
+
+public String getCustomerId(){
+return customerId;
+}
+
+
+public String getName(){
+return name;
+}
+
+
+
+
+}
